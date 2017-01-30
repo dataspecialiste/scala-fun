f76d1caf7829cc645fc5e80445d975a5d0ec2429
Mathusuthan Kannan <mathusuthankannan@gmail.com>
2017-01-30 17:31:06 +0530

    First Example Project

---
+++ example/src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+import example.Lists
+
+/**
+  * Created by mathusuthan on 1/30/2017.
+  */
+object Main extends App{
+  println(Lists.max(List(1,3,2, 10)))
+
+  println(Lists.sum(List(1,3,2, 10)))
+}
