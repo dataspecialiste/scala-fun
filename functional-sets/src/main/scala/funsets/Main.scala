89a70a02962640e3e746faef90ae2da2bdcda902
Mathusuthan Kannan <mathusuthan.ds@gmail.com>
2017-02-11 15:52:48 +0530

    Completed Functional Sets

--- functional-sets/src/main/scala/funsets/Main.scala
+++ functional-sets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,8 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  println(union(singletonSet(1), singletonSet(2)))
+
+  println(intersect(singletonSet(2), singletonSet(2)))
 }
